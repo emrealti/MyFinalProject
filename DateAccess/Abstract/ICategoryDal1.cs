30cd411ef2b9f1bf9fd83705478fe184f95d768b
emrealti <emre.a3461@gmail.com>
2023-09-28 11:30:11 +0300

    Add project files.

---
+++ DateAccess/Abstract/ICategoryDal1.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Concrete;
+
+namespace DateAccess.Abstract
+{
+    public interface ICategoryDal1
+    {
+        void Add(Category categoryt);
+        void Delete(Category category);
+        List<Category> GetAll();
+        List<Category> GetAllByCategory(int categoryId);
+        void Update(Category category);
+    }
+}
---
+++ DateAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        //generic constrain filtereleme sistemi 
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); 
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+       
+    }
+}
